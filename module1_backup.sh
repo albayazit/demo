cb534c18e0cbcf0bc0136a79178a484db96ac0c9
Bayazit Khasanov <57368931+albayazit@users.noreply.github.com>
2024-05-23 17:58:54 +0300

    Update module1_backup.sh

--- module1_backup.sh
+++ module1_backup.sh
@@ -1,6 +1,6 @@
 #!bin/bash
-backup_dir=”/etc”
-dest_dir=”/opt/backup”
+backup_dir="/etc"
+dest_dir="/opt/backup"
 mkdir -p $dest_dir
-tar -czf $dest_dir/$(hostname -s)-$(date +”%d.%m.%y”).tgz $backup_dir
-echo “Done!”
+tar -czf $dest_dir/$(hostname -s)-$(date +"%d.%m.%y").tgz $backup_dir
+echo "Done!"
