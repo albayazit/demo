83bfc1683ef19139d86b68a672a80c1d66d0e62d
albayazit <bakhasanov@gmail.com>
2024-05-24 00:11:00 +0300

    ISP router - network settings

--- isp.sh
+++ isp.sh
@@ -1,11 +1,13 @@
-echo "NAT interface (enp0sX):"
-read NAT
+echo "Настройка интерфейсов (enp0sX):"
 echo "ISP-HQ:"
-read ISP-HQ
+read ISPHQ
 echo "ISP-BR:"
-read ISP-BR
-echo "ISP-CLI"
-read ISP-CLI
+read ISPBR
+echo "ISP-CLI"ßß
+read ISPCLI
 
-echo -e "\nauto $NAT \niface $NAT inet dhcp\n" >> /etc/network/interfaces
-echo -e "\nauto $ISP-HQ \niface $ISP-HQ inet dhcp\n" >> /etc/network/interfaces
+echo -e "\nauto $ISPHQ \niface $ISPHQ inet static\naddress 2.2.2.1\nnetmask 255.255.255.0" >> /etc/network/interfaces
+
+echo -e "\nauto $ISPBR \niface $ISPBR inet static\naddress 3.3.3.1\nnetmask 255.255.255.0" >> /etc/network/interfaces
+
+echo -e "\nauto $ISPCLI \niface $ISPCLI inet static\naddress 10.10.10.1\nnetmask 255.255.255.0" >> /etc/network/interfaces
