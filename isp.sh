ca35ee8c45f073682f87c7e6fc516ad57e65466e
albayazit <bakhasanov@gmail.com>
2024-05-24 00:29:07 +0300

    ip-forwarding

--- isp.sh
+++ isp.sh
@@ -1,13 +1,20 @@
-echo "Настройка интерфейсов (enp0sX):"
+#!/bin/bash
+
+echo "Network config (enp0sX)..."
 echo "ISP-HQ:"
 read ISPHQ
 echo "ISP-BR:"
 read ISPBR
-echo "ISP-CLI"ßß
+echo "ISP-CLI"
 read ISPCLI
 
-echo -e "\nauto $ISPHQ \niface $ISPHQ inet static\naddress 2.2.2.1\nnetmask 255.255.255.0" >> /etc/network/interfaces
+echo -e "auto $ISPHQ \niface $ISPHQ inet static\naddress 2.2.2.1\nnetmask 255.255.255.0\n" >> /etc/network/interfaces
+echo -e "auto $ISPBR \niface $ISPBR inet static\naddress 3.3.3.1\nnetmask 255.255.255.0\n" >> /etc/network/interfaces
+echo -e "auto $ISPCLI \niface $ISPCLI inet static\naddress 10.10.10.1\nnetmask 255.255.255.0\n" >> /etc/network/interfaces
+systemctl restart networking
 
-echo -e "\nauto $ISPBR \niface $ISPBR inet static\naddress 3.3.3.1\nnetmask 255.255.255.0" >> /etc/network/interfaces
 
-echo -e "\nauto $ISPCLI \niface $ISPCLI inet static\naddress 10.10.10.1\nnetmask 255.255.255.0" >> /etc/network/interfaces
+echo "Turn on ip_forwarding..."
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
+
