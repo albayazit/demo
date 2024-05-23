0784204f605e8bda4873d52bd8d4d010f444fe47
albayazit <bakhasanov@gmail.com>
2024-05-24 01:11:19 +0300

    iptables configuration

--- isp.sh
+++ isp.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
 
+# Настройка сети
 echo "Network config (enp0sX)..."
 echo "ISP-HQ:"
 read ISPHQ
 echo "ISP-BR:"
 read ISPBR
-echo "ISP-CLI"
+echo "ISP-CLI:"
 read ISPCLI
-
 echo -e "auto $ISPHQ \niface $ISPHQ inet static\naddress 2.2.2.1\nnetmask 255.255.255.0\n" >> /etc/network/interfaces
 echo -e "auto $ISPBR \niface $ISPBR inet static\naddress 3.3.3.1\nnetmask 255.255.255.0\n" >> /etc/network/interfaces
 echo -e "auto $ISPCLI \niface $ISPCLI inet static\naddress 10.10.10.1\nnetmask 255.255.255.0\n" >> /etc/network/interfaces
 systemctl restart networking
 
-
+# Включение перенаправления пакетов
 echo "Turn on ip_forwarding..."
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
 
+# Установка iptables
+echo "Installing iptables..."
+apt-get install iptables -y
+
+# Установка правил для iptables
+echo "Creating iptables rules..."
+touch /etc/iptables.sh
+
+echo "#!/bin/bash" >> /etc/iptables.sh
+echo "export WAN="enpos3"" >> /etc/iptables.sh
+
+echo "iptables -F" >> /etc/iptables.sh
+echo "iptables -F -t nat" >> /etc/iptables.sh
+echo "iptables -F -t mangle" >> /etc/iptables.sh
+echo "iptables -X" >> /etc/iptables.sh
+echo "iptables -t nat -X" >> /etc/iptables.sh
+echo "iptables -t mangle -X" >> /etc/iptables.sh
+
+echo "iptables -P INPUT ACCEPT" >> /etc/iptables.sh
+echo "iptables -P OUTPUT ACCEPT" >> /etc/iptables.sh
+echo "iptables -P FORWARD ACCEPT" >> /etc/iptables.sh
+
+echo "iptables -t nat -A POSTROUTING -o $WAN -j MASQUERADE" >> /etc/iptables.sh
+
+echo "/sbin/iptables-save > /etc/iptables.rules" >> /etc/iptables.sh
