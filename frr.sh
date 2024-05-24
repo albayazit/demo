0b996e5189ddd5eabd9f25aafb9feea3b8bad000
albayazit <bakhasanov@gmail.com>
2024-05-25 00:17:46 +0300

    making frr

---
+++ frr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "FRR setup. Choose machine:"
+echo "1 - HQ-R; 2 - ISP; 3 - BR-R"
+read NUM
+
+apt-get install frr -y
+sed -i 's/ospfd=no/ospfd=yes/' /etc/frr/daemons
+
+if [[ $NUM -eq 1 ]]; then
+    ROUTER=10.10.10.1
+elif [[ $NUM -eq 2 ]]; then
+    echo "2";
+elif [[ $NUM -eq 3 ]]; then
+    echo "3"
+else
+    echo "INCORRECT INPUT"
+fi
+
+echo "router ospf" >> /etc/frr/frr.conf
