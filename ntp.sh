5afcbdc71a561c0d683b91cd1c9f2844675f286e
albayazit <bakhasanov@gmail.com>
2024-05-26 02:54:33 +0300

    Ntp fix, samba script

--- ntp.sh
+++ ntp.sh
@@ -3,23 +3,9 @@
 apt install chrony -y
 timedatectl set-timezone Europe/Moscow
 
-echo "What machine now?"
-echo "1 - HQ-R | 2 - Others"
-read MACHINE
-
 rm -rf /etc/chrony/chrony.conf
-
-if [[ $MACHINE -eq 1 ]]; then
-    wget albayazit.github.io/demo/chrony/chrony_hqr.conf
-    mv chrony_hqr.conf /etc/chrony/chrony.conf
-    systemctl enable --now chrony
-    chronyc sources
-elif [[ $MACHINE -eq 2 ]]; then
-    wget albayazit.github.io/demo/chrony/chrony.conf
-    mv chrony.conf /etc/chrony/chrony.conf
-    systemctl restart chrony
-    systemctl enable --now chrony
-    chronyc sources
-else
-    echo "INCORRECT INPUT"
-fi
+wget albayazit.github.io/demo/chrony/chrony.conf
+mv chrony_hqr.conf /etc/chrony/chrony.conf
+systemctl enable --now chrony
+chronyc sources
+echo "Write 'server 20.20.20.1 iburst prefer' on other machines."
