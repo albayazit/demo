a546f386d0a0fa80fa271be0012c40a9b8eea46e
albayazit <bakhasanov@gmail.com>
2024-05-26 02:36:55 +0300

    Fix file_server script

--- file_server.sh
+++ file_server.sh
@@ -3,15 +3,15 @@
 echo "SMB FILE SERVER CONF IN HQ-SRV..."
 apt-get install samba -y
 mkdir -p /mnt/files/{branch_files,network,admin_files}
+useradd admin -p P@ssw0rd
+useradd network_admin -p P@ssw0rd
+useradd branch_admin -p P@ssw0rd
 smbpasswd -a admin
 smbpasswd -a network_admin
 smbpasswd -a branch_admin
-wget albayazit.github.io/demo/samba/users
+wget albayazit.github.io/demo/samba/smbconf
 users >> /etc/samba/smb.conf
 rm users
 systemctl restart smbd.service
 echo "Done!"
 echo "On HQ-R: smbclient //20.20.20.2/admin_files -U admin"
-
-
-
