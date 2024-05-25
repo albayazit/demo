7f642af1f7bcc44c9601241de69d726731797345
albayazit <bakhasanov@gmail.com>
2024-05-25 03:19:34 +0300

    DNS configuration

--- dns.sh
+++ dns.sh
@@ -3,6 +3,23 @@ echo "Configurating DNS..."
 sed -i 's/\/\/ forwarders/forwarders/' /etc/bind/named.conf.options
 sed -i 's/\/\/\t0.0.0.0;/ 8.8.8.8;/' /etc/bind/named.conf.options
 sed -i 's/\/\/ };/};/' /etc/bind/named.conf.options
-wget albayazit.github.io/demo/named.conf.local
+wget albayazit.github.io/demo/dns/named.conf.local
 rm -rf /etc/bind/named.conf.local
 mv named.conf.local /etc/bind/named.local.conf
+wget albayazit.github.io/demo/dns/db.30.30.30
+wget albayazit.github.io/demo/dns/db.20.20.20
+wget albayazit.github.io/demo/dns/db.branch.work
+wget albayazit.github.io/demo/dns/db.hq.work
+
+mkdir /etc/bind/zones
+mv db.30.30.30 /etc/bind/zones/
+mv db.20.20.20 /etc/bind/zones/
+mv db.branch.work /etc/bind/zones/
+mv db.hq.work /etc/bind/zones/
+
+echo "Checking with named-checkconf..."
+named-checkconf
+named-checkconf -z
+echo "Deleting trash files..."
+systemctl restart bind9
+echo "Done! Add new DNS server '20.20.20.2' in /etc/reslov in all machines."
