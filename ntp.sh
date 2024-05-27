57a4e7a13812c1243ae5cee3d283d5d6b963130b
albayazit <bakhasanov@gmail.com>
2024-05-27 22:57:02 +0300

    Hot fix on ntp

--- ntp.sh
+++ ntp.sh
@@ -5,7 +5,7 @@ timedatectl set-timezone Europe/Moscow
 
 rm -rf /etc/chrony/chrony.conf
 wget albayazit.github.io/demo/chrony/chrony.conf
-mv chrony_hqr.conf /etc/chrony/chrony.conf
+mv chrony.conf /etc/chrony/chrony.conf
 systemctl enable --now chrony
 chronyc sources
 echo "Write 'server 20.20.20.1 iburst prefer' on other machines."
